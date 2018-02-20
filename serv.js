88dde18f1d1a0f2b1cdbbc2c332ab69e7c5566dd
Manjil Thapa <manjil@live.unc.edu>
2018-02-20 00:46:33 -0500

    [descriptive message]

--- serv.js
+++ serv.js
@@ -1,13 +1,29 @@
 const express = require('express'),
       path = require('path'),
+      mysql = require('mysql'),
       app = express();
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "523",
+  port:"8889"
+});
 
-let serv = app.listen(3000, '127.0.0.1', () => console.log('Example app listening localhost:3000!'));
-
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM questions", function (err, result, fields) {
+    if (err) throw err;
+    questions = JSON.stringify(result);
+    console.log(questions);
+  });
+  con.query("SELECT * FROM answers", function (err, result, fields) {
+    if (err) throw err;
+    answers = JSON.stringify(result);
+    console.log(answers);
+  });
+});
 
 app.get('/', (req, res) => {
     res.render('index');
