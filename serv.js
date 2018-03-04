022e80ccc4924678092f956004f15e61d1ca2f7f
Kacey Cleveland <k1996@ad.unc.edu>
2018-03-04 14:40:33 -0500

    cloudapps integration

--- serv.js
+++ serv.js
@@ -42,6 +42,19 @@ let config = {
 };
 */
 
+console.log(config);
+
+config.port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+config.ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+if (process.env.OPENSHIFT_MYSQL_PASSWORD) {
+    config.database.host = process.env.MYSQL_SERVICE_HOST;
+    config.database.port = process.env.MYSQL_SERVICE_PORT;
+    config.database.user = process.env.OPENSHIFT_MYSQL_USER;
+    config.database.password = process.env.OPENSHIFT_MYSQL_PASSWORD;
+    config.database.database = process.env.OPENSHIFT_MYSQL_DATABASE;
+}
+
 var con = mysql.createConnection({
     host: config.database.host,
     user: config.database.user,
@@ -49,6 +62,12 @@ var con = mysql.createConnection({
     database: config.database.database,
     port: config.database.port
 });
+
+con.query("SELECT * FROM questions", function(err, result, fields) {
+    if(err) {
+        console.log("Could not connect to host", config.database.host);
+    }
+})
 
 let serv = app.listen(config.port, config.ip, () => console.log('Example app listening ' + config.ip + ':' + config.port + '!'))
 
