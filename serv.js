d0e184c05f775e83f275a7be28ff8b926fcf63d2
Kacey Cleveland <k1996@ad.unc.edu>
2018-03-27 12:27:27 -0400

    added connection checks for db

--- serv.js
+++ serv.js
@@ -65,6 +65,20 @@ var con = mysql.createConnection({
     port: config.database.port
 });
 
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
 con.query("SELECT * FROM questions", function(err, result, fields) {
     if(err) {
         console.log("Could not connect to host", config.database.host);
