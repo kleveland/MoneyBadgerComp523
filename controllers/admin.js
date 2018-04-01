6323ac5a39b7a1160cca09111488081c01bb7269
Steven Nguyen <stevenn1296@yahoo.com>
2018-04-01 00:27:33 -0400

    quiz editor view

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,12 +2,12 @@ module.exports = function (app, dbuser) {
 
     app.get('/admin', (req, res) => {
         res.render('indexAdmin');
-      });
+    });
     app.get('/createQuiz', (req, res) => {
         res.render('quizCreate');
-        });
-
-    app.post('/create/quiz', (req, res) => {
+    });
+    app.post('/editQuiz', (req, res) => {
+        res.render('quizEditor');
         console.log("RECIEVED ANSWER", req.body);
-      });
-    }
+    });
+}
